f031db3d6bb545135fc4385ab7a8e9aeab245923
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-08 21:50:53 +0200

    Теперь всё работает на всех сайтах

--- onpageload.js
+++ onpageload.js
@@ -1,11 +1,9 @@
-// function run() {
+function run() {
     var imgs = document.getElementsByTagName("img")
-    console.log(123);
     for (var i = 0; i < imgs.length; i++) {
         img = imgs[i]
         img.src = "http://www.sickchirpse.com/wp-content/uploads/2012/04/Nic-Cages-Face.jpg"
     }
-// }
-// alert(document.ready)
-// document.addEventListener('DOMContentLoaded', run);
-// console.log("asdasd")
+}
+
+run();
