b556aa2a48f80a0ee65186c35ab18be76ccdf6dd
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-09 11:31:51 +0200

    Поддержка локальных изображений

--- onpageload.js
+++ onpageload.js
@@ -2,7 +2,8 @@ function run(event) {
     var imgs = document.getElementsByTagName("img");
     for (var i = 0; i < imgs.length; i++) {
         img = imgs[i];
-        img.src = "http://www.sickchirpse.com/wp-content/uploads/2012/04/Nic-Cages-Face.jpg";
+
+        img.src = chrome.extension.getURL('meme.jpg');
     }
 }
 
