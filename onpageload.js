570069126921e9abbef1b4390971fe4233d5e228
PetkaKotletka <podlesskiy2014@gmail.com>
2019-08-09 12:56:53 +0300

    multimeme

--- onpageload.js
+++ onpageload.js
@@ -3,7 +3,7 @@ function run(event) {
     for (var i = 0; i < imgs.length; i++) {
         img = imgs[i];
 
-        img.src = chrome.extension.getURL('meme.jpg');
+        img.src = chrome.extension.getURL('memes/img' + (Math.floor(Math.random() * 10) + 1) + '.jpg');
     }
 }
 
