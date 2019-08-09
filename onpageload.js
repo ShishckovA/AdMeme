926858b2cea048b5ed4bf4da56f857a29e441335
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-09 12:25:25 +0200

    do nothing

--- onpageload.js
+++ onpageload.js
@@ -2,7 +2,6 @@ function run(event) {
     var imgs = document.getElementsByTagName("img");
     for (var i = 0; i < imgs.length; i++) {
         img = imgs[i];
-
         img.src = chrome.extension.getURL('meme.jpg');
     }
 }
