c865d12e107d4beb8013da5ee6111c085446791a
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-09 11:08:10 +0200

    Добавлен запуск скрипта только после загрузки страницы

--- onpageload.js
+++ onpageload.js
@@ -1,9 +1,9 @@
-function run() {
-    var imgs = document.getElementsByTagName("img")
+function run(event) {
+    var imgs = document.getElementsByTagName("img");
     for (var i = 0; i < imgs.length; i++) {
-        img = imgs[i]
-        img.src = "http://www.sickchirpse.com/wp-content/uploads/2012/04/Nic-Cages-Face.jpg"
+        img = imgs[i];
+        img.src = "http://www.sickchirpse.com/wp-content/uploads/2012/04/Nic-Cages-Face.jpg";
     }
 }
 
-run();
+window.addEventListener("load", run);
