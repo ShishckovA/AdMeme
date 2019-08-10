e11f2f6cb69487998153cd1fcb35b8dad685701f
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-10 10:58:11 +0200

    Делюсь функциями

--- onpageload.js
+++ onpageload.js
@@ -1,10 +1,29 @@
-function run(event) {
-    var imgs = document.getElementsByTagName("img");
-    for (var i = 0; i < imgs.length; i++) {
-        img = imgs[i];
-
-        img.src = chrome.extension.getURL('memes/img' + (Math.floor(Math.random() * 10) + 1) + '.jpg');
+function deleteAdSence () {
+    //Lesha
+    var divs = document.getElementsByTagName("script");
+    for (var i = 0; i < divs.length; i++) {
+        div = divs[i];
+        srcURL = div.innerHTML;
+        if (srcURL.indexOf("adsbygoogle") != -1) {
+            div.outerHTML = "<img src=\"" + chrome.extension.getURL('memes/img' + (Math.floor(Math.random() * 10) + 1) + '.jpg') + "\">";
+            alert(div.id);
+        }
     }
 }
 
+function deleteBanners () {
+    //Natasha
+}
+
+function deleteFromVk() {
+    //Petka
+}
+
+
+function run(event) {
+    deleteAdSence();
+    deleteBanners();
+    deleteFromVk();
+}
+// run();
 window.addEventListener("load", run);
