41434e1e299642afcc3e92b82ecd0215cedcee93
Alexey Shishkov <liosha.shishckov@yandex.ru>
2019-08-10 12:57:57 +0200

    Только для гита, do nothing

--- onpageload.js
+++ onpageload.js
@@ -1,29 +1,11 @@
-function deleteAdSence () {
-    //Lesha
-    var divs = document.getElementsByTagName("script");
-    for (var i = 0; i < divs.length; i++) {
-        div = divs[i];
-        srcURL = div.innerHTML;
-        if (srcURL.indexOf("adsbygoogle") != -1) {
-            div.outerHTML = "<img src=\"" + chrome.extension.getURL('memes/img' + (Math.floor(Math.random() * 10) + 1) + '.jpg') + "\">";
-            alert(div.id);
-        }
-    }
-}
-
-function deleteBanners () {
-    //Natasha
-}
-
-function deleteFromVk() {
-    //Petka
-}
-
+function getImgSrc() {
+    return chrome.extension.getURL('memes/img' + (Math.floor(Math.random() * 10) + 1) + '.jpg')
+} 
 
 function run(event) {
     deleteAdSence();
     deleteBanners();
     deleteFromVk();
 }
-// run();
+
 window.addEventListener("load", run);
